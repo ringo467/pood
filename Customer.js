d60cd46278eaae3762bafce90816219b02705b48
ringo467 <ringo.fedotov@voco.ee>
2025-11-27 16:42:18 +0200

    Add files via upload

---
+++ Customer.js
@@ -0,0 +1,29 @@
+export class Customer {
+    constructor(name) {
+        this.name = name;
+        this.orderHistory = [];
+    }
+
+    placeOrder(cart) {
+        const newOrder = new Order(cart);
+        this.orderHistory.push(newOrder);
+        return newOrder;
+    }
+
+    printOrderHistory() {
+        console.log(`${this.name} tellimuse ajalugu`);
+
+        if (this.orderHistory.length === 0) {
+            console.log("Tellimusi ei ole");
+            return;
+        }
+
+        this.orderHistory.forEach((order, index) => {
+            console.log(`\nTellimus #${index + 1}`);
+            console.log("Kuupäev:", order.orderDate.toLocaleString());
+            console.log("Kogusumma:", order.cart.calculateTotal() + " €");
+        });
+
+        
+    }
+}
